80547618bf10908e4fea54dd801581771c05e1e1
khaoxkongz <thanapat.makkawal@gmail.com>
2024-03-19 21:31:02 +0700

    feat(main): initialize handlers and repository instances
    - Import required modules and classes
    - Create a new instance of PrismaClient
    - Initialize repository instances with the Prisma client
    - Create handler instances with respective repository instances
    
    This commit initializes the required instances for handlers and repositories, and wires them together to enable the application flow.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 
+import { newRepositoryClassroom, newRepositoryClub, newRepositoryStudent } from "./repositories";
+import { HandlerClassroom, HandlerClub, HandlerStudent } from "./handler";
+
 async function main() {
   const db = new PrismaClient();
   const repoClassroom = newRepositoryClassroom(db);
@@ -21,6 +24,18 @@ async function main() {
   server.use("/classrooms", routerClassroom);
   server.use("/students", routerStudent);
   server.use("/clubs", routerClub);
+
+  routerClassroom.post("/", handlerClassroom.createClassroom.bind(handlerClassroom));
+  routerClassroom.get("/", handlerClassroom.getClassrooms.bind(handlerClassroom));
+
+  routerStudent.post("/", handlerStudent.createStudent.bind(handlerStudent));
+  routerStudent.get("/", handlerStudent.getStudents.bind(handlerStudent));
+  routerStudent.get("/:id", handlerStudent.getStudentById.bind(handlerStudent));
+  routerStudent.post("/clubs/:id", handlerStudent.setClubs.bind(handlerStudent));
+  routerStudent.delete("/:id", handlerStudent.deleteStudentById.bind(handlerStudent));
+
+  routerClub.post("/", handlerClub.createClub.bind(handlerClub));
+  routerClub.get("/", handlerClub.getClubs.bind(handlerClub));
 
   server.listen(8000, () => console.log("starting server"));
 }
