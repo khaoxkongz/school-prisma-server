9f7d2c7271be3b775923180c8354c304a471a0de
khaoxkongz <thanapat.makkawal@gmail.com>
2024-03-26 13:27:53 +0700

    (refactor/repository): Refactor repository classes for teacher and course
    - Updated repository classes for teacher and course to use Prisma's latest API and features.
    - Changed method implementations in RepositoryTeacher and RepositoryCourse to match Prisma's new conventions.
    - Added include clauses in RepositoryTeacher and RepositoryCourse to fetch related data when querying.
    - Improved code readability and consistency in repository classes.

--- src/repositories/teacher.ts
+++ src/repositories/teacher.ts
@@ -1,26 +1,27 @@
-import { DbTeacherModel, TeacherDb } from "../../prisma";
-import { ICreateTeacher, ITeacherWithOutPassword, IWhereTeacher } from "../entities/teacher";
+import { DbDriver } from "../../prisma";
+import { ICreateTeacher, ITeacherWithCourse, ITeacherWithOutPassword, IWhereTeacher } from "../entities/teacher";
 import { IRepositoryTeacher } from "../interfaces/teacher";
 
 import modelTeacher from "../models/teacher";
 
 class RepositoryTeacher implements IRepositoryTeacher {
-  private db: TeacherDb;
+  private db: DbDriver;
 
-  constructor(db: TeacherDb) {
+  constructor(db: DbDriver) {
     this.db = db;
   }
 
   public createOne = async (data: ICreateTeacher): Promise<ITeacherWithOutPassword> => {
-    return await this.db.create({
+    return await this.db.teacher.create({
       data: modelTeacher.toCreateTeacherDataFromDto(data),
       select: modelTeacher.defaultTeacherSelected(),
     });
   };
 
-  public findUnique = async (where: IWhereTeacher): Promise<DbTeacherModel> => {
-    return await this.db.findUniqueOrThrow({
+  public findUnique = async (where: IWhereTeacher): Promise<ITeacherWithCourse> => {
+    return await this.db.teacher.findUniqueOrThrow({
       where: modelTeacher.fromUniqueDataToTeacher(where),
+      include: modelTeacher.includeCoursesModel(),
     });
   };
 }
