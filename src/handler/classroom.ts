f367bc307813bcb7a9488cb8883b2d9f4030aa68
khaoxkongz <thanapat.makkawal@gmail.com>
2024-03-26 19:13:14 +0700

    refactor(handlers/classroom): update classroom handler for better error handling

--- src/handler/classroom.ts
+++ src/handler/classroom.ts
@@ -1,18 +1,23 @@
-import { Request, Response } from "express";
-import { IRepositoryClassroom } from "../repositories";
+import { IHandlerClassroom } from "../routes/classroom";
+import { IRepositoryClassroom } from "../interfaces/classroom";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-export class HandlerClassroom {
+export class HandlerClassroom implements IHandlerClassroom {
   private repo: IRepositoryClassroom;
 
   constructor(repo: IRepositoryClassroom) {
     this.repo = repo;
   }
 
-  async createClassroom(req: Request, res: Response): Promise<Response> {
+  public createClassroom: IHandlerClassroom["createClassroom"] = async (req, res) => {
     const { name } = req.body;
 
     if (!name) {
       return res.status(400).json({ error: `missing name in body` }).end();
+    }
+
+    if (name !== "string" || name.length < 0) {
+      return res.status(400).json({ error: "name must be empty" }).end();
     }
 
     return this.repo
@@ -22,19 +27,22 @@ export class HandlerClassroom {
         const errMsg = `failed to create classroom ${name}`;
         console.error(`${errMsg}: ${err}`);
 
-        return res.status(500).json({ error: errMsg }).end();
-      });
-  }
+        if (err instanceof PrismaClientKnownRequestError && err.code === "P2002") {
+          return res.status(400).json({ error: errMsg });
+        }
 
-  async getClassrooms(req: Request, res: Response): Promise<Response> {
+        return res.status(500).json({ error: "Internal Server Error" }).end();
+      });
+  };
+
+  public getClassrooms: IHandlerClassroom["getClassrooms"] = async (req, res) => {
     return this.repo
       .getClassrooms()
       .then((classroom) => res.status(200).json(classroom).end())
       .catch((err) => {
         const errMsg = `failed to get classrooms`;
         console.error(`${errMsg}: ${err}`);
-
-        return res.status(500).json({ err: errMsg }).end();
+        return res.status(500).json({ error: "Internal Server Error" }).end();
       });
-  }
+  };
 }
